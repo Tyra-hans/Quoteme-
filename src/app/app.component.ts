66608a9f9ee27e7c0eda4e17e6ba2857f6dd13d5
Tyra-hans <tyrahans17@gmail.com>
2019-09-22 07:50:32 +0300

    add quotes to quotes array in app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Quote } from './quote';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +9,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Quote-me';
 
-    quotes: string[];
+  quotes: Quote = [
+    {id: 1, name: 'When you cant find the sunshine, be the sunshine'},
+    {id: 2, name: 'The grass is greener where you water it'},
+    {id: 3, name: 'Wherever life plants you bloom with grace'},
+    {id: 4, name: 'Learn from yesterday,live for today, hope for tomorrow'},
+    {id: 5, name: 'Take time to do what makes your soul happy'},
+    {id: 6, name: 'The sun is a daily reminder that we too can rise from the darkness and shine our own light'},
+  ];
 
     constructor() {
-      this.quotes = ['When you cant find the sunshine, be the sunshine',
-      'The grass is greener where you water it',
-       'Wherever life plants you bloom with grace'];
+      this.quotes = [];
     }
   }
